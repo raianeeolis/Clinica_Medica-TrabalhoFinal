156f858601446eb75c5b7f171663d0facfb4f656
raianeeolis <raianeoliveiraa0711@gmail.com>
2025-11-05 14:43:59 -0300

    Renomeando

--- SistemaHospitalar/src/main/java/ucb/estudo/view/ClinicaMedica.java
+++ SistemaHospitalar/src/main/java/ucb/estudo/view/ClinicaMedica.java
@@ -4,7 +4,7 @@
 
 package ucb.estudo.view;
 
-public class SistemaHospitalar {
+public class ClinicaMedica {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
